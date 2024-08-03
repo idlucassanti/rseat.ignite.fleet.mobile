f2b3eb061ccc751998d3e478790b53cacede8fb7
Lucas Santi <idlucassanti@hotmail.com>
2024-08-03 02:40:35 -0300

    create page SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from 'react-native';
-import { styles } from './styles';
+import backgroundImage from '../../assets/background.png';
+import { Button } from '../../components/Button';
+import { Container, Title, Slogan } from "./styles";
 
 export function SignIn() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container source={backgroundImage}>
+      <Title>Ignite Fleet</Title>
+      <Slogan>Gestão de uso de veículos</Slogan>
+
+      <Button title='Entrar com o Google' />
+    </Container>
   );
 }
