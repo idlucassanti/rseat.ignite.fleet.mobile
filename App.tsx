460a3cea98e3d40d38db6be4c778fa06c98fd9a5
Lucas Santi <idlucassanti@hotmail.com>
2024-08-03 01:25:31 -0300

    install and configure expo-font

--- App.tsx
+++ App.tsx
@@ -1,9 +1,22 @@
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/theme/index';
 
 import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <SignIn />
